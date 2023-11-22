2955888ca395e5a764cba31f4831a349e95b94ec
RaulVictor-m <raulvictor822@gmail.com>
2023-11-22 16:22:20 -0300

    small fixes on fmt and some change in colors display

--- src/main.zig
+++ src/main.zig
@@ -14,12 +14,12 @@ const R = 0;
 const G = 1;
 const B = 2;
 
-const tile_height = 256;
-const tile_width = 256;
+const tile_height = 100;
+const tile_width  = 100;
 
 //size in tiles
-const screen_height_tl = 30;
-const screen_width_tl  = 30;
+const screen_height_tl = 50;
+const screen_width_tl  = 50;
 
 //size in pixels
 const screen_height_px = tile_height * screen_height_tl;
@@ -29,7 +29,7 @@ const screen_width_px  = tile_width  * screen_width_tl;
 const screen_height_bt = screen_height_px * 3; 
 const screen_width_bt  = screen_width_px  * 3;
 
-var screen: [screen_height_bt * screen_width_bt * 3]u8 = undefined;
+var screen: [screen_height_bt * screen_width_bt]u8 = undefined;
 
 //the generated wang tiles map
 var grid: [screen_height_tl * screen_width_tl]u4 = undefined;
@@ -38,9 +38,14 @@ var grid: [screen_height_tl * screen_width_tl]u4 = undefined;
 
 //triangle frag
 pub fn frag(pos: Vec2, mask: u4) Vec3 { //recive x, y -- returns rgb
-    @setFloatMode(.Optimized);
+    //@setFloatMode(.Optimized);
+
+    //puts black square in the middle
+    // const mid_dist = @fabs(pos - @as(Vec2, @splat(0.5)));
+    // if(mid_dist[0] < 0.3 and mid_dist[1] < 0.3) return Color.black;
+
     const colors = [_]Vec3{
-    (Color.red + Color.green),
+    (Color.red + Color.blue),
     (Color.red) 
     };
 
@@ -54,7 +59,7 @@ pub fn frag(pos: Vec2, mask: u4) Vec3 { //recive x, y -- returns rgb
     [_]f32{0.5,1.0}  //botton
     };
 
-    const d = 0.4;
+    const d = 0.5;
     const d_diff = 0.5 / d;
     const sides_dist: [2]Vec2 = [_]Vec2 {
     [_]f32{d, d*d_diff}, //horizontal increase Y
@@ -75,9 +80,21 @@ pub fn frag(pos: Vec2, mask: u4) Vec3 { //recive x, y -- returns rgb
 
         //painting if in range
         if (n_dist < 1){
-            const sin = 1 - (@sin(n_dist * (std.math.pi / 2.0)));
-            //const sin = 1 - (@sin(n_dist * (1.0 / 2.0) / @sin(1 * (1.0 / 2.0))));
-            const gradient: Vec3 = @splat(sin);
+            //harder sin gradiant
+            // const sin = 1 - (@sin(n_dist * (std.math.pi / 2.0)));
+            // const gradient: Vec3 = @splat(sin);
+
+            //softer sin gradiant
+            // const sin = 1 - (@sin(n_dist * (1.0 / 2.0) / @sin(1 * (1.0 / 2.0))));
+            // const gradient: Vec3 = @splat(sin);
+
+            //cos function gradiant
+            // const cos = (@cos(n_dist * (std.math.pi / 2.0)));
+            // const gradient: Vec3 = @splat(cos);
+
+            //raw gradient
+            const gradient: Vec3 = @splat(0.7);
+
             return colors[m] * gradient;
         }
     }
@@ -85,15 +102,18 @@ pub fn frag(pos: Vec2, mask: u4) Vec3 { //recive x, y -- returns rgb
 
 }
 
+var randomizer: std.rand.DefaultPrng = undefined;
 pub fn generateRandTile(values_mask: u4, pos_mask :u4) u4{
-    //TODO: implement proper rand 
-    const rand_num = @as(u4, @truncate(@as(u128, @bitCast(std.time.nanoTimestamp()))));
+
+    const rand_num = randomizer.random().int(u4);//@as(u4, @truncate(@as(u128, std.time.nanoTimestamp()))); //time
 
     //0b1100 -- bot right
     //0b0011 -- top left
     return (rand_num & (~(pos_mask >> 2))) | ((values_mask >> 2));
 }
 pub fn generateGrid() void {
+    randomizer = std.rand.DefaultPrng.init(@bitCast(std.time.milliTimestamp()));
+
     //0b1100 -- bot right
     //0b0011 -- top left
     for (&grid, 0..) |*mask,i| {
@@ -109,9 +129,11 @@ pub fn generateGrid() void {
                 const x = i - raw_y;
 
                 break: blk if (i % screen_width_tl == 0)
-                    generateRandTile(grid[raw_y - screen_width_tl + x] & 0b1000 , 0b1000)
+                    generateRandTile(grid[raw_y - screen_width_tl + x] & 0b1000 ,
+                                                                         0b1000)
                 else 
-                    generateRandTile((grid[raw_y - screen_width_tl + x] & 0b1000) | (grid[raw_y + x - 1] & 0b0100), 0b1100);
+                    generateRandTile((grid[raw_y - screen_width_tl + x] & 0b1000) |
+                                          (grid[raw_y + x - 1] & 0b0100), 0b1100);
                
             },
         };
@@ -123,10 +145,14 @@ pub fn drawImageToFile(file_path: []const u8) !void{
     defer file.close();
 
     var buffer: [100]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
 
     // writting the P6 header
-    const bitmap_file_header = try std.fmt.allocPrint(fba.allocator(), "P6\n{d} {d}\n255\n", .{screen_height_px, screen_width_px});
+    const bitmap_file_header = try std.fmt.bufPrint( &buffer,
+                                                     "P6\n{d} {d}\n255\n",
+                                                     .{
+                                                         screen_width_px,
+                                                         screen_height_px
+                                                     });
     _ = try file.write(bitmap_file_header);
 
     var cursor: usize = 0;
@@ -140,9 +166,7 @@ pub fn drawImageToFile(file_path: []const u8) !void{
             const yf = @as(f32, @floatFromInt(y)) / tile_height;
 
             // getting to the fake fragment shader
-            const pos = ((row / tile_height) * screen_width_tl  + col / tile_width);
-            //std.debug.print("{d} \n", .{grid[pos]});
-
+            const pos = (((row/tile_height) * screen_width_tl) + (col/tile_width));
             var rgb = frag(.{xf,yf}, grid[pos]);
 
             // mapping it back the color definition
@@ -163,7 +187,7 @@ pub fn main() !void {
 
     generateGrid();
 
-    try drawImageToFile(&file_name);
+    try drawImageToFile(file_name);
 
     std.debug.print("end...\n", .{});
 
